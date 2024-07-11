396736ba852a6e9d49b85d7a9ee3814f6487dbb5
tealingg <tealingg@tealingg.dev>
2024-07-11 22:38:33 +0100

    Initial Commit

---
+++ src/lib/ContactCard.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let name = "Name";
+    export let icon = "fa-regular fa-user";
+    export let accountName = "@tealingg";
+    export let colour = "#06b6d4";
+    export let link = "";
+</script>
+
+<style>
+
+#card:hover {
+    border-color: var(--border-color);
+}
+
+</style>
+
+<a id="card" class="flex flex-row transition items-center gap-6 min-w-56 ease-linear duration-100 bg-zinc-800 px-6 py-4 border-transparent border-2" style="--border-color: {colour}" href={link} target="_blank">
+    <i class="text-3xl {icon}" style="color: {colour}"></i>
+    <div class="flex flex-col">
+        <p class="font-bold text-xl" style="color: {colour}">{name}</p>
+        <p>{accountName}</p>
+    </div>
+</a>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+
+import Particles, { particlesInit } from "@tsparticles/svelte";
+import { loadSlim } from "@tsparticles/slim";
+
+import ContactCard from "$lib/ContactCard.svelte";
+
+void particlesInit(async (engine) => {
+    await loadSlim(engine)
+})
+
+</script>
+
+<Particles
+    id="tsparticles"
+    url="particles.json"
+/>
+
+<div class="flex relative gap-12 flex-col z-10 min-h-screen min-w-screen justify-center items-center">
+    <div class="transition ease-linear duration-100 bg-zinc-800 text-4xl font-bold p-6 border-transparent border-4 hover:border-cyan-500">
+        <h1>tealingg<span class="text-neutral-500">.dev</span></h1>
+    </div>
+    <div class="flex flex-row flex-wrap justify-center max-w-3xl gap-8">
+        <ContactCard name="Discord" icon="fa-brands fa-discord" colour="#7785CC" link="https://discord.com" />
+        <ContactCard name="GitHub" icon="fa-brands fa-github" link="https://github.com/tealingg" />
+        <ContactCard name="Twitch" icon="fa-brands fa-twitch" colour="#9146FF" link="https://twitch.tv/tealingg" />
+        <ContactCard name="YouTube" icon="fa-brands fa-youtube" colour="#FF0000" link="https://youtube.com/@tealingg" />
+    </div>
+    <div class="transition ease-linear duration-100 bg-zinc-800 p-4 border-transparent border-4 hover:border-cyan-500">
+        <h1 class="text-neutral-500">more coming soon...</h1>
+    </div>
+</div>
